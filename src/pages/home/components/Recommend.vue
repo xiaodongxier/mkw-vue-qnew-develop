6e632fa3ed3104bef65aab58ba4d172d832ec774
小东西儿 <xiaodongxier@gmail.com>
2023-01-11 17:59:06 +0800

    热销推荐组件开发

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>热销推荐</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -4,6 +4,7 @@
     <home-header></home-header>
     <home-swiper></home-swiper>
     <home-icons></home-icons>
+    <home-recommend></home-recommend>
   </div>
 </template>
 
@@ -11,12 +12,14 @@
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
+import HomeRecommend from './components/Recommend'
 export default {
   name: 'Home',
   components: {
     HomeHeader: HomeHeader,
     HomeSwiper: HomeSwiper,
-    HomeIcons: HomeIcons
+    HomeIcons: HomeIcons,
+    HomeRecommend: HomeRecommend
   }
 }
 </script>
