d0841ee8f4700e2bf08b306918000df63e0810d5
小东西儿 <xiaodongxier@gmail.com>
2023-01-12 16:05:08 +0800

    数据循环输出

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -2,11 +2,11 @@
   <div>
     <div class="title">热销推荐</div>
     <ul>
-      <li class="item border-bottom">
-        <img class="item-img" src="https://imgs.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_200x200_1bc99086.jpg" alt="">
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <img class="item-img" :src="item.imgUrl" alt="">
         <div class="item-info">
-          <p class="item-title">故宫博物院</p>
-          <p class="item-desc">世界五大宫之首，穿越与您近在咫尺</p>
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
           <button class="item-buttom">查看详情</button>
         </div>
       </li>
@@ -16,7 +16,49 @@
 
 <script>
 export default {
-  name: 'HomeRecommend'
+  name: 'HomeRecommend',
+  data: function () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl: 'https://imgs.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_200x200_1bc99086.jpg',
+          title: '故宫博物院',
+          desc: '世界五大宫之首，穿越与您近在咫尺'
+        },
+        {
+          ia: '0002',
+          imgUrl: 'https://imgs.qunarzz.com/sight/p0/2005/39/3979f1867defec4ea3.water.jpg_200x200_325a015d.jpg',
+          title: '八达岭长城',
+          desc: '不到长城非好汉'
+        },
+        {
+          ia: '0003',
+          imgUrl: 'https://qimgs.qunarzz.com/piao_qsight_provider_piao_qsight_web/0100j1200087e9wb39FE1.jpg_200x200_e3180970.jpg',
+          title: '颐和园',
+          desc: '中国现存最大的皇家园林'
+        },
+        {
+          ia: '0004',
+          imgUrl: 'https://imgs.qunarzz.com/sight/p0/1507/cc/19733fc0135062788140cbb48ae606a7.water.jpg_200x200_d9ebe2fd.jpg',
+          title: '北京野生动物园',
+          desc: '敢与森林之王近距离接触吗？'
+        },
+        {
+          ia: '0005',
+          imgUrl: 'https://imgs.qunarzz.com/sight/p0/1505/f5/f5f45e1a83537bcb.water.jpg_200x200_444cc7e8.jpg',
+          title: '圆明园',
+          desc: '百年皇家的珍宝,民族耻辱的见证'
+        },
+        {
+          ia: '0006',
+          imgUrl: 'https://imgs.qunarzz.com/sight/p0/201405/12/51b63883c25f5d87af3c08bb016e2bd7.jpg_200x200_8eb33f07.jpg',
+          title: '恭王府',
+          desc: '一座恭王府，半部清代史'
+        }
+      ]
+    }
+  }
 }
 </script>
 
@@ -38,7 +80,7 @@ export default {
   .item-info
     flex: 1
     padding: .1rem
-    min-width:0
+    min-width: 0
     .item-title
       line-height: .54rem
       font-size: .32rem
