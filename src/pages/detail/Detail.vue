1f4f09aa20572ea48cd228c3c631a1f2bf552d48
小东西儿 <xiaodongxier@gmail.com>
2023-03-16 17:36:24 +0800

    递归组件使用

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,18 +2,61 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner.vue'
 import DetailHeader from './components/Header.vue'
+import DetailList from './components/List.vue'
 export default {
   name: 'Detail',
+  data () {
+    return {
+      list: [
+        {
+          title: '儿童',
+          children: [
+            {
+              title: '儿童1班',
+              children: [
+                {
+                  title: '儿童1-1班',
+                  children: [
+                    {
+                      title: '儿童1-1-1班'
+                    },
+                    {
+                      title: '儿童1-1-2班'
+                    }
+                  ]
+                },
+                {
+                  title: '儿童1-2班'
+                }
+              ]
+            },
+            {
+              title: '儿童2班'
+            }
+          ]
+        },
+        {
+          title: '成人'
+        },
+        {
+          title: '老人'
+        }
+      ]
+    }
+  },
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
   }
 }
 </script>
